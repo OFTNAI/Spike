a1bcd62a0f297b97abe83f4de665a13b6663bd73
James Isbister <isbisterjb@gmail.com>
2016-06-07 13:18:18 +0100

    Minor changes to experiment

--- Experiments/ConductanceExperiment1.cpp
+++ Experiments/ConductanceExperiment1.cpp
@@ -196,10 +196,10 @@ int main (int argc, char *argv[]){
 	// TESTING UNTRAINED
 	float presentation_time_per_stimulus_per_epoch = 0.2f;
 	bool save_spikes = false;
-	SpikeAnalyser * untrained_spike_analyser = new SpikeAnalyser(simulator.neurons, (ImagePoissonSpikingNeurons*)simulator.input_neurons);
-	simulator.RunSimulationToCountNeuronSpikesForSingleCellAnalysis(presentation_time_per_stimulus_per_epoch, temp_model_type, save_spikes, untrained_spike_analyser);
+	SpikeAnalyser * spike_analyser_for_untrained_network = new SpikeAnalyser(simulator.neurons, (ImagePoissonSpikingNeurons*)simulator.input_neurons);
+	simulator.RunSimulationToCountNeuronSpikesForSingleCellAnalysis(presentation_time_per_stimulus_per_epoch, temp_model_type, save_spikes, spike_analyser_for_untrained_network);
 	int number_of_bins = 3;
-	untrained_spike_analyser->calculate_single_cell_information_scores_for_neuron_group(EXCITATORY_NEURONS_LAYER_4, number_of_bins);
+	spike_analyser_for_untrained_network->calculate_single_cell_information_scores_for_neuron_group(EXCITATORY_NEURONS_LAYER_4, number_of_bins);
 
 
 	// TRAINING
@@ -212,8 +212,9 @@ int main (int argc, char *argv[]){
 	// TESTING TRAINED
 	presentation_time_per_stimulus_per_epoch = 1.0f;
 	save_spikes = false;
-	SpikeAnalyser * trained_spike_analyser = new SpikeAnalyser(simulator.neurons, (ImagePoissonSpikingNeurons*)simulator.input_neurons);
-	simulator.RunSimulationToCountNeuronSpikesForSingleCellAnalysis(presentation_time_per_stimulus_per_epoch, temp_model_type, save_spikes, trained_spike_analyser);
+	SpikeAnalyser * spike_analyser_for_trained_network = new SpikeAnalyser(simulator.neurons, (ImagePoissonSpikingNeurons*)simulator.input_neurons);
+	simulator.RunSimulationToCountNeuronSpikesForSingleCellAnalysis(presentation_time_per_stimulus_per_epoch, temp_model_type, save_spikes, spike_analyser_for_trained_network);
+	spike_analyser_for_trained_network->calculate_single_cell_information_scores_for_neuron_group(EXCITATORY_NEURONS_LAYER_4, number_of_bins);
 
 
 	clock_t end_entire_experiment = clock();
