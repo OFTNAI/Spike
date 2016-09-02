fixed a bug of simulator (recorded spikes were not refleshed). added a tool to plot average firing rate map. revised the way to bin the spikes in the infoAnalysis.py

PythonCode/SpikePlot.py
@@ -4,46 +4,158 @@ import numpy as np
 
 
 class SpikePlot(object):
-    def plotSpikes(self,phases,saveImage = True, showImage = True):
+    def loadParams(self,borrowed_globals):
+        globals().update(borrowed_globals);
+    
+    
+    def plotSpikes(self,phases,saveImage = True, showImage = True, nLayers=4):
+        plotAvgFR = True;
+        plotSpikes = True;
         
-        nLayers = 4;
-        exDim = 64;
-        inDim = 32;
+#         nLayers = 4;
+
+#         nObj = 3;
+#         nTrans = 2;
+#         nLayers = 4;
+#         presentationTime = 3.0;
+# 
+#                 
+#         exDim = 64;
+#         inDim = 32;
+        nExcitCells = exDim*exDim;#32*32;
+        nInhibCells = inDim*inDim;#16*16;
         
 #         fn_id = "../Results/Neurons_Epoch0_SpikeIDs.bin";
 #         fn_t = "../Results/Neurons_Epoch0_SpikeTimes.bin";
 
-        for phase in phases:
-#             fn_id = "../output/Neurons_SpikeIDs_" + phase + "_Epoch0.txt";
-#             fn_t = "../output/Neurons_SpikeTimes_" + phase + "_Epoch0.txt";
-#             spikeIDs = np.loadtxt(fn_id);
-#             spikeTimes = np.loadtxt(fn_t);
+        if (plotSpikes):
+            for phase in phases:
+    #             fn_id = "../output/Neurons_SpikeIDs_" + phase + "_Epoch0.txt";
+    #             fn_t = "../output/Neurons_SpikeTimes_" + phase + "_Epoch0.txt";
+    #             spikeIDs = np.loadtxt(fn_id);
+    #             spikeTimes = np.loadtxt(fn_t);
+        
+                fn_id = "../output/Neurons_SpikeIDs_" + phase + "_Epoch0.bin";
+                fn_t = "../output/Neurons_SpikeTimes_" + phase + "_Epoch0.bin";  
+                dtIDs = np.dtype('int32');
+                dtTimes = np.dtype('f4');
+                
+                spikeIDs = np.fromfile(fn_id, dtype=dtIDs);
+                spikeTimes = np.fromfile(fn_t, dtype=dtTimes);
     
-            fn_id = "../output/Neurons_SpikeIDs_" + phase + "_Epoch0.bin";
-            fn_t = "../output/Neurons_SpikeTimes_" + phase + "_Epoch0.bin";  
-            dtIDs = np.dtype('int32');
-            dtTimes = np.dtype('f4');
+                
+                #fig = plt.figure(0 , figsize=(300, 150),dpi=150);
+                fig = plt.figure(0 , figsize=(30, 15),dpi=150);
+        
+        
+                     
+                               
+                plt.plot(spikeTimes,spikeIDs,'.',color='k',markersize=2);
+                plt.hold(True)
+                for l in range(nLayers):
+                    plt.plot((0,np.max(spikeTimes)),(l*(exDim*exDim+inDim*inDim),l*(exDim*exDim+inDim*inDim)),'k-', color='k', linewidth=2.0);
+                    plt.plot((0,np.max(spikeTimes)),(l*(exDim*exDim+inDim*inDim)+exDim*exDim,l*(exDim*exDim+inDim*inDim)+exDim*exDim),'k--', color='r', linewidth=2.0);
+                
+                plt.xlabel('time [s]');
+                plt.xlim((0,np.max(spikeTimes)))
+                plt.ylim((0,nLayers*(exDim*exDim+inDim*inDim)))
+                
+                fig.savefig("../output/rasterPlot_"+phase+".png");
+        #         fig.savefig("../output/rasterPlot.eps");
+                print("figure rasterPlot.png is exported in output")
+                plt.clf();
             
-            spikeIDs = np.fromfile(fn_id, dtype=dtIDs);
-            spikeTimes = np.fromfile(fn_t, dtype=dtTimes);
-
+        if plotAvgFR:
+            for phase in phases:
+#                 fig=plt.figure(4 , figsize=(20, 5),dpi=150);
+                zoomConst = 3;
+                fig=plt.figure(4 , figsize=(zoomConst*2.5*nTrans*nObj, zoomConst*2*nLayers), dpi=1000);
+                
+                fn_id = "../output/Neurons_SpikeIDs_" + phase + "_Epoch0.bin";
+                fn_t = "../output/Neurons_SpikeTimes_" + phase + "_Epoch0.bin";  
+                dtIDs = np.dtype('int32');
+                dtTimes = np.dtype('f4');
+                
+                spikeIDs = np.fromfile(fn_id, dtype=dtIDs);
+                spikeTimes = np.fromfile(fn_t, dtype=dtTimes);
+                
+                
+                #plot Excitatory
+                FR = np.zeros((nObj, nTrans,nLayers, nExcitCells));
+                for l in range(nLayers):
+                    cond_ids = (l*(nExcitCells+nInhibCells) < spikeIDs) & (spikeIDs < l*(nInhibCells+nExcitCells)+nExcitCells);
+                    spikeIDs_layer = np.extract(cond_ids, spikeIDs);
+                    spikeTimes_layer = np.extract(cond_ids, spikeTimes);
+                    for obj in range(nObj):
+                        for trans in range(nTrans):
+                            cond_stim = ((presentationTime*(obj*nTrans+trans)) < spikeTimes_layer) & (spikeTimes_layer < (presentationTime*(obj*nTrans+trans+1)));
+                            spikeIDs_stim = np.extract(cond_stim,spikeIDs_layer)-((nExcitCells+nInhibCells)*l);
+                            for id in spikeIDs_stim:
+                                FR[obj,trans,l,id]=FR[obj,trans,l,id]+1;
+                FR/=presentationTime;
+    #             FR = np.random.rand(nObj, nTrans,nLayers, nExcitCells)
             
-            #fig = plt.figure(0 , figsize=(300, 150),dpi=150);
-            fig = plt.figure(0 , figsize=(30, 15),dpi=150);
-    
-            plt.plot(spikeTimes,spikeIDs,'.',color='k',markersize=2);
-            for l in range(nLayers):
-                plt.plot((0,np.max(spikeTimes)),(l*(exDim*exDim+inDim*inDim),l*(exDim*exDim+inDim*inDim)),'k-', color='k', linewidth=2.0);
-                plt.plot((0,np.max(spikeTimes)),(l*(exDim*exDim+inDim*inDim)+exDim*exDim,l*(exDim*exDim+inDim*inDim)+exDim*exDim),'k--', color='r', linewidth=2.0);
+                for obj in range(nObj):
+                    for l in range(nLayers):
+                        for trans in range(nTrans):
+                            plt.subplot(nLayers, nTrans*nObj, (nLayers-l-1)*(nTrans*nObj)+(obj*nTrans)+trans+1);
+                            plt.title('Firing Rate Map: obj ' + str(obj) )
+                            
+                            FRMap = np.zeros((exDim,exDim));
+                            for y in range(exDim):
+                                for x in range(exDim):
+                                    id = x*exDim + y;
+                                    FRMap[y,x] = FR[obj,trans,l,id];
+#                             Rmax = np.max(FRMap);
+                            Rmax = 100;
+                            plt.imshow(FRMap, cmap='jet', interpolation='none', vmin=0, vmax=Rmax)
+                            plt.colorbar();
+#                     plt.show();
+                fig.savefig("../output/AvgFR_"+phase+".png");
+                fig.savefig("../output/AvgFR_"+phase+".eps");
+                print("figure AvgFR.png is exported in output")
+                plt.clf();
+                
+                
+                
+                fig=plt.figure(4 , figsize=(zoomConst*2.5*nTrans*nObj, zoomConst*2*nLayers), dpi=1000);
+                #plot inhib
+                FR = np.zeros((nObj, nTrans,nLayers, nInhibCells));
+                for l in range(nLayers):
+                    cond_ids = (l*(nExcitCells+nInhibCells)+nExcitCells < spikeIDs) & (spikeIDs < (l+1)*(nInhibCells+nExcitCells));
+                    spikeIDs_layer = np.extract(cond_ids, spikeIDs);
+                    spikeTimes_layer = np.extract(cond_ids, spikeTimes);
+                    for obj in range(nObj):
+                        for trans in range(nTrans):
+                            cond_stim = ((presentationTime*(obj*nTrans+trans)) < spikeTimes_layer) & (spikeTimes_layer < (presentationTime*(obj*nTrans+trans+1)));
+                            spikeIDs_stim = np.extract(cond_stim,spikeIDs_layer)-((nExcitCells+nInhibCells)*l+nExcitCells);
+                            for id in spikeIDs_stim:
+                                FR[obj,trans,l,id]=FR[obj,trans,l,id]+1;
+                FR/=presentationTime;
+    #             FR = np.random.rand(nObj, nTrans,nLayers, nExcitCells)
             
-            plt.xlabel('time [s]');
-            plt.xlim((0,np.max(spikeTimes)))
-            plt.ylim((0,nLayers*(exDim*exDim+inDim*inDim)))
+                for obj in range(nObj):
+                    for l in range(nLayers):
+                        for trans in range(nTrans):
+                            plt.subplot(nLayers, nTrans*nObj, (nLayers-l-1)*(nTrans*nObj)+(obj*nTrans)+trans+1);
+                            plt.title('Firing Rate Map: obj ' + str(obj) )
+                            
+                            FRMap = np.zeros((inDim,inDim));
+                            for y in range(inDim):
+                                for x in range(inDim):
+                                    id = x*inDim + y;
+                                    FRMap[y,x] = FR[obj,trans,l,id];
+#                             Rmax = np.max(FRMap);
+                            Rmax = 100;
+                            plt.imshow(FRMap, cmap='jet', interpolation='none', vmin=0, vmax=Rmax)
+                            plt.colorbar();
+#                     plt.show();
+                fig.savefig("../output/AvgFR_"+phase+"_inhib.png");
+                fig.savefig("../output/AvgFR_"+phase+"_inhib.eps");
+                print("figure AvgFR_inhib.png is exported in output")
+                plt.clf();
             
-            fig.savefig("../output/rasterPlot_"+phase+".png");
-    #         fig.savefig("../output/rasterPlot.eps");
-            print("figure rasterPlot.png is exported in Results")
-            plt.hold(True)
+#         
             
             
         