ba4592c8a69be843dc5d14fecefd61d35912554e
Nasir Ahmad <nasir.y.ahm@gmail.com>
2016-10-20 21:09:44 +0100

    First set of documentation comments added. Doxygen config file added.

--- Neurons/Neurons.h
+++ Neurons/Neurons.h
@@ -1,56 +1,93 @@
-//	Neurons Class Header
-//	Neurons.h
-//
-//	Author: Nasir Ahmad
-//	Date: 7/12/2015
-//
-//  Adapted from NeuronPopulations by Nasir Ahmad and James Isbister
-//	Date: 6/4/2016
-
+/* \brief The most abstract Neuron class from which methods and attributes are inherited by SpikingNeurons etc.
+*/
 #ifndef Neurons_H
 #define Neurons_H
+
+/**
+ * @file   Neurons.h
+ * @brief  The most abstract Neuron class from which methods and attributes are inherited by SpikingNeurons etc.
+ *
+ */
 
 #include <cuda.h>
 #include <stdio.h>
 #include <vector_types.h>
 
 
-struct neuron_parameters_struct {
-	neuron_parameters_struct() {}
-
-	int group_shape[2];
-};
-
 #define PRESYNAPTIC_IS_INPUT( id ) (id < 0 ? true : false)
 #define CORRECTED_PRESYNAPTIC_ID(id, is_input) (is_input ? (-1 * (id)) - 1 : id) 
 
+/*!
+	This struct accompanies the neuron class and is used to provide the number of neurons 
+	(in a 2D grid) that are to be added to the total population.
+	This is also used as the foundation for the more sophisticated neuron parameter structs 
+	for SpikingNeurons etc.
+*/
+struct neuron_parameters_struct {
+	neuron_parameters_struct() {}
+
+	int group_shape[2];		/**< An int array with 2 values describing the 2D shape of a neuron group */
+};
+
+/*!
+	This is the parent class for SpikingNeurons.
+	It provides a set of default methods which are primarily used to add groups of neurons to the total population.
+*/
 class Neurons{
-public:
-	// Constructor/Destructor
+public:/**
+     *  Initializes pointers and variables.
+     */
+	
 	Neurons();
 	~Neurons();
 
-	int total_number_of_neurons;
-	int total_number_of_groups;
+	int total_number_of_neurons;				/**< Tracks the total neuron population size. */
+	int total_number_of_groups;					/**< Tracks the number of groups (the total neuron population is split into groups e.g. layers or excitatory/inh). */
+	int number_of_neurons_in_new_group;			/**< Keeps the number of neurons in the latest group. */
 
-	int **group_shapes;
-	int *start_neuron_indices_for_each_group;
-	int *last_neuron_indices_for_each_group;
+	int **group_shapes;							/**< The 2D shape of each group. */
+	int *start_neuron_indices_for_each_group;	/**< Indices of the beginnings of each group in the total population. */	
+	int *last_neuron_indices_for_each_group;	/**< Indices of the final neuron in each group. */
 
-	int number_of_neurons_in_new_group;
+	float* d_current_injections;				/**< Device array for the storage of current to be injected into each neuron on each timestep. */
 
-	float* d_current_injections;
+	dim3 number_of_neuron_blocks_per_grid;		/**< CUDA Device number of blocks */
+	dim3 threads_per_block;						/**< CUDA Device number of threads */
 
-	dim3 number_of_neuron_blocks_per_grid;
-	dim3 threads_per_block;
-
-
+	/**  
+     *  Determines the total number of neurons by which the simulation should increase.
+     This is a virtual function to allow polymorphism in the methods of various SpikingNeuron implementations.
+     	Allocates memory as necessary for group size and indices storage.
+		\param group_params A neuron_parameters_struct instance describing a 2D neuron population size.
+		\return The unique ID for the population which was requested for creation.
+     */
 	virtual int AddGroup(neuron_parameters_struct * group_params);
+
+	/**  
+     *  Exclusively for the allocation of device memory. This class requires allocation of d_current_injections only.
+		\param maximum_axonal_delay_in_timesteps The length (in timesteps) of the largest axonal delay in the simulation. Unused in this class.
+		\param high_fidelity_spike_storage A flag determining whether a bit mask based method is used to store spike times of neurons (ensure no spike transmission failure).
+     */
 	virtual void allocate_device_pointers(int maximum_axonal_delay_in_timesteps, bool high_fidelity_spike_storage);
+	
+	/**  
+     *  Unused in this class. Allows copying of static data related to neuron dynamics to the device.
+	*/
 	virtual void copy_constants_to_device();
+
+	/**  
+     *  Resets any undesired device based data which is dynamically reassigned during a simulation. In this case, the current to be injected at the next time step.
+	*/
 	virtual void reset_neuron_activities();
 	
+	/**  
+     *  A local, non-polymorphic function called by Neurons::reset_neuron_activities to reset Neurons::d_current_injections.
+	*/
 	void reset_current_injections();
+
+	/**  
+     *  A local, non-polymorphic function called in to determine the CUDA Device thread (Neurons::threads_per_block) and block dimensions (Neurons::number_of_neuron_blocks_per_grid).
+	*/
 	void set_threads_per_block_and_blocks_per_grid(int threads);
 };
 
