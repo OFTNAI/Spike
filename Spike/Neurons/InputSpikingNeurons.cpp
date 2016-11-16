6b6c9d3cac1677af728f6dcdb1bc26fbe828e394
Toby Smithe <git@tsmithe.net>
2016-11-16 00:06:37 +0000

    More CUDA backend split progress

--- Spike/Neurons/InputSpikingNeurons.cpp
+++ Spike/Neurons/InputSpikingNeurons.cpp
@@ -1,40 +1,20 @@
 #include "InputSpikingNeurons.hpp"
 #include <stdlib.h>
 #include <algorithm>
-//CUDA #include "../Helpers/CUDAErrorCheckHelpers.hpp"
 #include "../Helpers/TerminalHelpers.hpp"
 
-
-// InputSpikingNeurons Constructor
-InputSpikingNeurons::InputSpikingNeurons() {
-
-	total_number_of_transformations_per_object = 0;
-	total_number_of_objects = 0;
-	total_number_of_input_stimuli = 0;
-
-	current_stimulus_index = 0;
-}
-
-
-// InputSpikingNeurons Destructor
-InputSpikingNeurons::~InputSpikingNeurons() {
-}
-
 int InputSpikingNeurons::AddGroup(neuron_parameters_struct * group_params){
-	int new_group_id = SpikingNeurons::AddGroup(group_params);
-	return (-1*new_group_id - 1);
+  int new_group_id = SpikingNeurons::AddGroup(group_params);
+  return (-1*new_group_id - 1);
 };
 
-
-void InputSpikingNeurons::reset_neuron_activities() {
-
-	SpikingNeurons::reset_neuron_activities();
-
+void InputSpikingNeurons::reset() {
+  SpikingNeurons::reset();
+  backend.reset();
 }
 
-
 bool InputSpikingNeurons::stimulus_is_new_object_for_object_by_object_presentation(int stimulus_index) {
-	return true;
+  return true;
 }
 
 
--- Spike/Neurons/InputSpikingNeurons.hpp
+++ Spike/Neurons/InputSpikingNeurons.hpp
@@ -1,36 +1,31 @@
 #ifndef InputSpikingNeurons_H
 #define InputSpikingNeurons_H
 
-/*CUDA
-#include <cuda.h>
-#include <curand.h>
-#include <curand_kernel.h>
-*/
-
 #include "SpikingNeurons.hpp"
 
 struct input_spiking_neuron_parameters_struct : spiking_neuron_parameters_struct {
-
 };
+
+namespace Backend {
+  class InputSpikingNeurons : public SpikingNeurons {
+  public:
+    virtual void reset() {};
+  };
+} 
 
 class InputSpikingNeurons : public SpikingNeurons {
 public:
-	// Constructor/Destructor
-	InputSpikingNeurons();
-	~InputSpikingNeurons();
-
+  Backend::InputSpikingNeurons backend;
 	
-	int current_stimulus_index;
+  int current_stimulus_index = 0;
 
-	int total_number_of_input_stimuli;
-	int total_number_of_objects;
-	int total_number_of_transformations_per_object;
+  int total_number_of_input_stimuli = 0;
+  int total_number_of_objects = 0;
+  int total_number_of_transformations_per_object = 0;
 
-	virtual int AddGroup(neuron_parameters_struct * group_params);
-	virtual void reset_neuron_activities();
-	virtual bool stimulus_is_new_object_for_object_by_object_presentation(int stimulus_index);
-
+  virtual int AddGroup(neuron_parameters_struct * group_params);
+  virtual bool stimulus_is_new_object_for_object_by_object_presentation(int stimulus_index);
+  virtual void reset();
 };
-
 
 #endif
