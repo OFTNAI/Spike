494fb5ee696a12a95b7c3287bee0211d7799eec0
Toby Smithe <git@tsmithe.net>
2016-12-09 23:23:35 +0000

    Switch default device type to CUDA if available; now to fix runtime errors and TODO items

--- Spike/Backend/Context.cpp
+++ Spike/Backend/Context.cpp
@@ -7,7 +7,11 @@ namespace Backend {
     if (!_global_ctx)
       _global_ctx = new Context;
     // TODO: Add other device types (esp CUDA)
+#ifdef SPIKE_WITH_CUDA
+    _global_ctx->device = SPIKE_DEVICE_CUDA;
+#else
     _global_ctx->device = SPIKE_DEVICE_DUMMY;
+#endif
   }
 
   Context* get_current_context() {
