65a1b3863c5f85cbf6977c598c04939e74e4733a
Toby Smithe <git@tsmithe.net>
2017-01-13 11:55:48 +0000

    Implement simple backend fallback to Dummy if requested backend unavailable; print error message in debug mode.

--- Spike/Backend/Backend.hpp
+++ Spike/Backend/Backend.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "Context.hpp"
+#include "Macros.hpp"
 
 #include <cassert>
 #include <map>
 #include <string>
+
+#ifndef NDEBUG
+#include <iostream>
+#endif
 
 namespace Backend {
   class SpikeBackendBase {
@@ -28,7 +33,15 @@ namespace Backend {
     factory_map_t map;
     inline factory_t*& operator[](const std::string& name) {
       factory_t*& factory = map[name];
-      assert("Backend factory not registered!" && factory != nullptr);
+      if (factory == nullptr) {
+        factory = map["Dummy"];
+#ifndef NDEBUG
+        std::cerr << "Spike ERROR: Backend " << name
+                  << " not registered for " << typeid(FrontT).name() << ".\n"
+                  << "Spike ERROR: Falling back to Dummy..." << std::endl;
+#endif
+      }
+      assert(factory != nullptr);
       return factory;
     }
     inline factory_t*& operator[](std::string&& name) {
