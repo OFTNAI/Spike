809e8b903e09770243b9050278a23a147fcb6741
Nasir Ahmad <nasir.y.ahm@gmail.com>
2017-01-20 13:01:40 +0000

    Addition of vanRossum multiplicative STDP rule and fix for masq. rule.

---
+++ Spike/Backend/CUDA/STDP/vanRossumSTDP.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Spike/STDP/vanRossumSTDP.hpp"
+#include "STDP.hpp"
+
+#include "Spike/Backend/CUDA/Neurons/SpikingNeurons.hpp"
+#include "Spike/Backend/CUDA/Synapses/SpikingSynapses.hpp"
+
+#include "Spike/Backend/CUDA/CUDABackend.hpp"
+#include <cuda.h>
+#include <vector_types.h>
+#include <curand.h>
+#include <curand_kernel.h>
+
+namespace Backend {
+  namespace CUDA {
+    class vanRossumSTDP : public virtual ::Backend::CUDA::STDP,
+                           public virtual ::Backend::vanRossumSTDP {
+    public:
+      int* index_of_last_afferent_synapse_to_spike = nullptr;
+      bool* isindexed_ltd_synapse_spike = nullptr;
+      int* index_of_first_synapse_spiked_after_postneuron = nullptr;
+
+      ~vanRossumSTDP() override;
+      SPIKE_MAKE_BACKEND_CONSTRUCTOR(vanRossumSTDP);
+      using ::Backend::vanRossumSTDP::frontend;
+
+      void prepare() override;
+      void reset_state() override;
+
+      void allocate_device_pointers();
+      void apply_stdp_to_synapse_weights(float current_time_in_seconds) override;
+    };
+
+    // Kernel to carry out LTP/LTD
+    __global__ void vanrossum_apply_stdp_to_synapse_weights_kernel
+    (int* d_postsyns,
+     float* d_last_spike_time_of_each_neuron,
+     bool* d_stdp,
+     float* d_time_of_last_spike_to_reach_synapse,
+     float* d_synaptic_efficacies_or_weights,
+     int* d_index_of_last_afferent_synapse_to_spike,
+     bool* d_isindexed_ltd_synapse_spike,
+     int* d_index_of_first_synapse_spiked_after_postneuron,
+     struct vanrossum_stdp_parameters_struct stdp_vars,
+     float currtime,
+     size_t total_number_of_post_neurons);
+
+    __global__ void vanrossom_get_indices_to_apply_stdp
+    (int* d_postsyns,
+     float* d_last_spike_time_of_each_neuron,
+     float* d_time_of_last_spike_to_reach_synapse,
+     int* d_index_of_last_afferent_synapse_to_spike,
+     bool* d_isindexed_ltd_synapse_spike,
+     int* d_index_of_first_synapse_spiked_after_postneuron,
+     float currtime,
+     int* d_stdp_synapse_indices,
+     size_t total_number_of_stdp_synapses);
+  }
+}
---
+++ Spike/STDP/vanRossumSTDP.cpp
@@ -0,0 +1,48 @@
+//  vanRossum STDP Class C++
+//  vanRossumSTDP.cu
+//
+//  Author: Nasir Ahmad
+//  Date: 03/10/2016
+
+
+#include "vanRossumSTDP.h"
+#include "../Helpers/TerminalHelpers.hpp"
+
+vanRossumSTDP::~vanRossumSTDP() {
+  free(index_of_last_afferent_synapse_to_spike);
+  free(isindexed_ltd_synapse_spike);
+  free(index_of_first_synapse_spiked_after_postneuron);
+}
+
+void vanRossumSTDP::prepare_backend_late() {
+  // Add the correct space for last synapse
+  index_of_last_afferent_synapse_to_spike = (int*)malloc(sizeof(int)*neurs->total_number_of_neurons);
+  isindexed_ltd_synapse_spike = (bool*)malloc(sizeof(bool)*neurs->total_number_of_neurons);
+  index_of_first_synapse_spiked_after_postneuron = (int*)malloc(sizeof(int)*neurs->total_number_of_neurons);
+
+  // Initialize indices
+  for (int i=0; i < neurs->total_number_of_neurons; i++){
+    index_of_last_afferent_synapse_to_spike[i] = -1;
+    isindexed_ltd_synapse_spike[i] = false;
+    index_of_first_synapse_spiked_after_postneuron[i] = -1;
+  }
+}
+
+// Implementation of the STDP Rule for Irina's Model
+void vanRossumSTDP::Set_STDP_Parameters(SpikingSynapses* synapses, SpikingNeurons* neurons, SpikingNeurons* input_neurons, stdp_parameters_struct* stdp_parameters){
+	stdp_params = (masquelier_stdp_parameters_struct *)stdp_parameters;
+	syns = synapses;
+	neurs = neurons;
+}
+
+// Run the STDP
+void vanRossumSTDP::Run_STDP(float current_time_in_seconds, float timestep){
+  apply_stdp_to_synapse_weights(current_time_in_seconds);
+}
+
+void vanRossumSTDP::apply_stdp_to_synapse_weights(float current_time_in_seconds) {
+  backend()->apply_stdp_to_synapse_weights(current_time_in_seconds);
+}
+
+SPIKE_MAKE_INIT_BACKEND(vanRossumSTDP
+);
