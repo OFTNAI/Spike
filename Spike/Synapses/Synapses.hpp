7e15cafc7b7b12ea7edbaf4f68e921b3868ca421
Nasir Ahmad <nasir.y.ahm@gmail.com>
2017-01-20 11:38:54 +0000

    Synapse Class Docs introduced from nas_master.

--- Spike/Synapses/Synapses.hpp
+++ Spike/Synapses/Synapses.hpp
@@ -1,11 +1,14 @@
-// Synapse Class Header
-// Synapse.h
-//
-//	Author: Nasir Ahmad
-//	Date: 7/12/2015
+/* \brief The most abstract Synapses class from which methods and attributes are inherited by SpikingSynapses etc.
+*/
 
 #ifndef SYNAPSES_H
 #define SYNAPSES_H
+
+/**
+ * @file   Synapses.h
+ * @brief  The most abstract Synapses class from which methods and attributes are inherited by SpikingSynapses etc.
+ *
+ */
 
 #include "Spike/Base.hpp"
 
@@ -48,6 +51,10 @@ namespace Backend {
 
 static_assert(std::has_virtual_destructor<Backend::Synapses>::value, "contract violated");
 
+/*!
+  This enum contains the possible connectivity types which can be requested
+  when adding synapstic connections between neuron populations.
+*/
 enum CONNECTIVITY_TYPE
 {
   CONNECTIVITY_TYPE_ALL_TO_ALL,
@@ -57,6 +64,12 @@ enum CONNECTIVITY_TYPE
   CONNECTIVITY_TYPE_SINGLE
 };
 
+/*!
+  This struct accompanies the synapses class and is used to provide the a number
+  of parameters to control synapse creation.
+  This is also used as the foundation for the more sophisticated neuron parameter structs
+  for SpikingSynapses etc.
+*/
 struct synapse_parameters_struct {
   int max_number_of_connections_per_pair = 1;
   int pairwise_connect_presynaptic;
@@ -69,7 +82,10 @@ struct synapse_parameters_struct {
   int connectivity_type = CONNECTIVITY_TYPE_ALL_TO_ALL;
 };
 
-
+/*!
+  This is the parent class for SpikingSpiking.
+  It provides a set of default methods which are primarily used to add groups of synapses to the connectivity.
+*/
 class Synapses : public virtual SpikeBase {
 
 public:
@@ -82,19 +98,31 @@ public:
   void prepare_backend_early() override;
   
   // Variables
-  int total_number_of_synapses = 0;
-  int temp_number_of_synapses_in_last_group = 0;
-  int largest_synapse_group_size = 0;
-  bool print_synapse_group_details = false;
+  int total_number_of_synapses = 0;                 /**< Tracks the total number of synapses in the connectivity */
+  int temp_number_of_synapses_in_last_group = 0;    /**< Tracks the number of synapses in the last added group. */
+  int largest_synapse_group_size = 0;               /**< Tracks the size of the largest synaptic group. */
+  bool print_synapse_group_details = false;         /**< A flag used to indicate whether group details should be printed */
 	
   // Host Pointers
-  int* presynaptic_neuron_indices = nullptr;
-  int* postsynaptic_neuron_indices = nullptr; 
-  int* original_synapse_indices = nullptr;
-  int* synapse_postsynaptic_neuron_count_index = nullptr;
-  float* synaptic_efficacies_or_weights = nullptr;
+  int* presynaptic_neuron_indices = nullptr;                /**< Indices of presynaptic neuron IDs */
+  int* postsynaptic_neuron_indices = nullptr;               /**< Indices of postsynaptic neuron IDs */
+  int* original_synapse_indices = nullptr;                  /**< Indices by which to order the presynaptic and postsynaptic neuron IDs if a shuffle is carried out */
+  int* synapse_postsynaptic_neuron_count_index = nullptr;   /**< An array of the number of incoming synapses to each postsynaptic neuron */
+  float* synaptic_efficacies_or_weights = nullptr;          /**< An array of synaptic efficacies/weights accompanying the pre/postsynaptic_neuron_indices */
 
   // Functions
+
+  /**
+   *  Determines the synaptic connections to add to the simulation connectivity.
+   This is a virtual function to allow polymorphism in the methods of various sub-class implementations.
+    Allocates memory as necessary for group size and indices storage.
+  \param presynaptic_group_id An int ID of the presynaptic neuron population (ID is < 0 if presynaptic population is an InputSpikingNeuron population)
+  \param postsynaptic_group_id An int ID of the postsynaptic neuron population.
+  \param neurons A pointer to an instance of the Neurons class (or sub-class) which is included in this simulation.
+  \param input_neurons A pointer to an instance of the Neurons class (or sub-class) which is included in this simulation (for population indices < 0, i.e. InputSpikingNeurons).
+  \param timestep A float indicating the timestep at which the simulator is running.
+  \param synapse_params A synapse_parameters_struct pointer describing the connectivity arrangement to be added between the pre and postsynaptic neuron populations.
+   */
   virtual void AddGroup(int presynaptic_group_id, 
                         int postsynaptic_group_id, 
                         Neurons * neurons,
@@ -102,7 +130,15 @@ public:
                         float timestep,
                         synapse_parameters_struct * synapse_params);
 
+  /**
+     *  A function called in to reallocate memory for a given number more synapses.
+     /param increment The number of synapses for which allocated memory must be expanded.
+  */
   virtual void increment_number_of_synapses(int increment);
+
+  /**
+     *  A function to shuffle the order of synapse array storage for increased GPU memory write speeds. Currently Unused.
+  */
   virtual void shuffle_synapses();
 
   void reset_state() override;
