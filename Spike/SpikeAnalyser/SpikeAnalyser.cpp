6424440d2e91c7ff7492709e5ae50b6e772a471d
Toby Smithe <git@tsmithe.net>
2017-04-12 12:41:19 +0100

    Fix silly bugs

--- Spike/SpikeAnalyser/SpikeAnalyser.cpp
+++ Spike/SpikeAnalyser/SpikeAnalyser.cpp
@@ -114,7 +114,7 @@ void SpikeAnalyser::calculate_fitness_score(float optimal_average_firing_rate, f
 			if (average_number_of_neuron_spikes_per_second_for_neuron_group < optimal_average_firing_rate) {
 				neuron_group_score_based_on_avg = maxScore * (1- (optimal_average_firing_rate - average_number_of_neuron_spikes_per_second_for_neuron_group)/optimal_average_firing_rate);
 			} else {
-				neuron_group_score_based_on_avg = maxScore/(abs(average_number_of_neuron_spikes_per_second_for_neuron_group - optimal_average_firing_rate)+1);
+				neuron_group_score_based_on_avg = maxScore/(fabs(average_number_of_neuron_spikes_per_second_for_neuron_group - optimal_average_firing_rate)+1);
 			}
 //			neuron_group_score = 100.0/(abs(average_number_of_neuron_spikes_per_second_for_neuron_group - optimal_average_firing_rate)+1);
 
@@ -122,7 +122,7 @@ void SpikeAnalyser::calculate_fitness_score(float optimal_average_firing_rate, f
 			if (max_number_of_neuron_spikes_per_second_for_neuron_group < optimal_max_firing_rate) {
 				neuron_group_score_based_on_max = maxScore * (1- (optimal_max_firing_rate - max_number_of_neuron_spikes_per_second_for_neuron_group)/optimal_max_firing_rate);
 			} else {
-				neuron_group_score_based_on_max = maxScore/(abs(max_number_of_neuron_spikes_per_second_for_neuron_group - optimal_max_firing_rate)+1);
+				neuron_group_score_based_on_max = maxScore/(fabs(max_number_of_neuron_spikes_per_second_for_neuron_group - optimal_max_firing_rate)+1);
 			}
 		}
 		
