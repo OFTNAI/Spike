f8bdd62b6fd003bc8e6925559696294bedfbbddb
Nasir Ahmad <nasiryahm@gmail.com>
2017-05-24 17:02:54 +0100

    Addition of default output in CreateDirectory

--- Spike/Simulator/Simulator.cpp
+++ Spike/Simulator/Simulator.cpp
@@ -100,11 +100,12 @@ Simulator::~Simulator(){
 }
 
 void Simulator::CreateDirectoryForSimulationDataFiles(string directory_name_for_simulation_data_files) {
+	mkdir(("output/"),S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
 	if (mkdir(("output/"+directory_name_for_simulation_data_files).c_str(),S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH)==0)
 		printf("\nDirectory created\n");
 	else
 		print_message_and_exit("\nERROR: You must set a different experiment name to avoid overwriting the results\n");
-	full_directory_name_for_simulation_data_files = "output/"+directory_name_for_simulation_data_files+"/";
+	// full_directory_name_for_simulation_data_files = "output/"+directory_name_for_simulation_data_files+"/";
 }
 
 
